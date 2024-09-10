de090d5afdd01eda3fbec896790e245e9c78c97b
wflentje <wflentje@appliedres.com>
2024-09-10 09:08:24 -0600

    Samba test container and refactor user manager tests to be stateless (#29)
    * Samba test container support
    
    * Refactor tests to be stateless.
    
    ---------
    
    Co-authored-by: John Bauer <jbauer@appliedres.com>

--- tests/run.sh
+++ tests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name dev-ad --hostname ldap.schneide.dev --privileged -p 636:636 -e SMB_ADMIN_PASSWORD=admin123! -v $PWD/:/opt/ad-scripts -v $PWD/samba-data:/var/lib/samba dev-ad
+docker run --name dev-ad --hostname ldap.schneide.dev --privileged -p 636:636 -e SMB_ADMIN_PASSWORD=admin123! -v $PWD/:/opt/ad-scripts -v $PWD/samba-data:/var/lib/samba appliedres/dev-ad
